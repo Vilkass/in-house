c8636fbc3ab1177053a26f185626a8e96fbb9def
Vilkass <antankas@gmail.com>
2022-05-25 11:43:47 +0300

    Database + FXML update
    - Added DB connection class
    - Added Main window FXML file
    - Added authentication FXML file
    - Added JavaFX dependencies

---
+++ src/main/java/controller/MainWindowController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class MainWindowController {
+
+
+
+
+}
